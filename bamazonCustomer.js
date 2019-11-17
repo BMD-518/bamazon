a7ee79c5e42b593dfafa62032fabdf368b9c98d3
Bijan Dayani <bdayani518@gmail.com>
2019-11-17 18:57:37 -0500

    created inventory function, installed easy-table module

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 // Require Modules
 var mysql = require('mysql');
+var Table = require('easy-table');
 
 
 
@@ -13,3 +14,28 @@ var connection = mysql.createConnection({
 });
 
 // 
+connection.connect(function(err) {
+    if(err) throw err;
+    showInventory();
+});
+
+
+function showInventory() {
+    // create sql query to pull database table info
+    var query = "SELECT * FROM products";
+    connection.query(query, function(err, res){
+        if (err) throw err;
+        // Create table with easy-table
+        var t = new Table;
+        // Select key values to display in table
+        res.forEach(function (product){
+            t.cell('Item ID', product.item_id);
+            t.cell('Product Name', product.product_name);
+            t.cell('Price', product.price);
+            t.newRow()
+        })
+        // Console log table
+        console.log(t.toString())
+    })
+};
+
